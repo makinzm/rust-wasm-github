d28a045c129fd6fa16788e615a7076374034e877
makinzm <nozomi.maki.da@gmail.com>
2024-08-12 22:57:34 +0900

    refactor: click to see in detailed

--- src/distribution.rs
+++ src/distribution.rs
@@ -7,6 +7,34 @@ use rust_wasm_github::distributions::discrete::poisson::PoissonDistribution;
 
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+struct DistributionProps {
+    name: &'static str,
+    children: Children,
+}
+
+#[function_component(DistributionItem)]
+fn distribution_item(props: &DistributionProps) -> Html {
+    let visible = use_state(|| false);
+    let toggle_visibility = {
+        let visible = visible.clone();
+        Callback::from(move |_| visible.set(!*visible))
+    };
+
+    html! {
+        <li>
+            <div onclick={toggle_visibility} style="cursor: pointer;">
+                { if *visible { format!("{} ▼", props.name) } else { format!("{} ►", props.name) } }
+            </div>
+            if *visible {
+                <div style="margin-left: 20px;">
+                    { for props.children.iter() }
+                </div>
+            }
+        </li>
+    }
+}
+
 #[function_component(Distribution)]
 pub fn distribution() -> Html {
     html! {
@@ -15,15 +43,27 @@ pub fn distribution() -> Html {
             <div>
                 <h2>{"🤯 Discrete Distributions" }</h2>
                 <ul>
-                    <li> <PoissonDistribution /> </li>
-                    <li> <GeometricDistribution /> </li>
-                    <li> <NegativeBinomialDistribution /> </li>
-                    <li> <HypergeometricDistribution /> </li>
+                    <DistributionItem name="Poisson Distribution">
+                        <PoissonDistribution />
+                    </DistributionItem>
+                    <DistributionItem name="Geometric Distribution">
+                        <GeometricDistribution />
+                    </DistributionItem>
+                    <DistributionItem name="Negative Binomial Distribution">
+                        <NegativeBinomialDistribution />
+                    </DistributionItem>
+                    <DistributionItem name="Hypergeometric Distribution">
+                        <HypergeometricDistribution />
+                    </DistributionItem>
                 </ul>
                 <h2>{"😂 Continuous Distributions" }</h2>
                 <ul>
-                    <li> <ExponentialDistribution /> </li>
-                    <li> <BivariateNormalDistribution /> </li>
+                    <DistributionItem name="Exponential Distribution">
+                        <ExponentialDistribution />
+                    </DistributionItem>
+                    <DistributionItem name="Bivariate Normal Distribution">
+                        <BivariateNormalDistribution />
+                    </DistributionItem>
                 </ul>
             </div>
         </>
